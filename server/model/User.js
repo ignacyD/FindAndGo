feeb239756978ff192555113836c482033133f0e
Ignacy <ignacencjusz@gmail.com>
2023-03-15 13:08:29 +0100

    database created

---
+++ server/model/User.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    password: String,
+    email: String,
+    createdAt: Date,
+});
+
+const User = model('User', userSchema);
+
+
+module.exports = User;
