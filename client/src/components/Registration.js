1f747b6c63f10a87dfc7ff1fa2ff4107b5a33cd8
Ignacy <ignacencjusz@gmail.com>
2023-03-15 09:42:04 +0100

    Registration and login components created

--- client/src/components/Registration.js
+++ client/src/components/Registration.js
@@ -1,7 +1,78 @@
+import { useState } from 'react'
+
 function Registration() {
-    return(
+    const [registrationData, setRegistrationData] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        retypePassword: ""
+    })
+
+    const updateData = e => {
+        const fieldName = e.target.name
+        setRegistrationData(existingValues => ({
+            ...existingValues,
+            [fieldName]: e.target.value,
+        }))
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(registrationData)
+    }
+
+    return (
         <div className="registration">
-            Test Registration
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <input
+                        type="text"
+                        placeholder="First name"
+                        name='firstName'
+                        value={registrationData.firstName}
+                        onChange={updateData}
+                    >
+                    </input>
+                </label>
+                <label>
+                    <input
+                        type="text"
+                        placeholder="Last name"
+                        name='lastName'
+                        value={registrationData.lastName}
+                        onChange={updateData}
+                    ></input>
+                </label>
+                <label>
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        name='email'
+                        value={registrationData.email}
+                        onChange={updateData}
+                    ></input>
+                </label>
+                <label>
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name='password'
+                        value={registrationData.password}
+                        onChange={updateData}
+                    ></input>
+                </label>
+                <label>
+                    <input
+                        type="password"
+                        placeholder="Re-type password"
+                        name='retypePassword'
+                        value={registrationData.retypePassword}
+                        onChange={updateData}
+                    ></input>
+                </label>
+                <button type="submit">Submit</button>
+            </form>
         </div>
     )
 }
