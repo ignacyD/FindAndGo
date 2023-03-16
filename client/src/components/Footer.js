3e8db10fe1d4d182c4d536939badfacdcd2dbba9
Beata Zomer <beatazomer@MacBook-Air.local>
2023-03-16 13:35:37 +0100

    footer and header stage 3

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,9 +4,9 @@ function Footer() {
 
     return (
         <div className="footer">
-            <p>Address: 40 Collage Gardens, KT3 6NT New Malden, London</p>
-            <p>Phone: +44 7595 288 651</p>
-            <p>Email: contact@findandgo.co.uk</p>
+            <p className="footer_text">Address: 40 Collage Gardens, KT3 6NT London</p>
+            <p className="footer_text">Phone: +44 7595 288 651</p>
+            <p className="footer_text">Email: contact@findandgo.co.uk</p>
         </div>
     )
 }
