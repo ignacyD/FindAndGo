050d8af5f5383b557675e27b694078e93cfc9595
Beata Zomer <beatazomer@MacBook-Air.local>
2023-03-16 08:49:27 +0100

    Footer first stage

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,9 +1,19 @@
+import "./Footer.css"
+import React from 'react';
 function Footer() {
-    return(
-        <div className="footer">
-            Test Footer
-        </div>
+
+
+    return (
+        <footer>
+            <div className="footer">
+                <h3>Contact us</h3>
+                <p>Address: 40 Collage Gardens, KT3 6NT New Malden, London</p>
+                <p>Phone: +44 7595 288 651</p>
+                <p>contact@findandgo.co.uk</p>
+            </div>
+        </footer>
     )
 }
 
 export default Footer;
+
