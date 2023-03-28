3e90028b7879d0ae8d45d61223d3490bd1fcaa44
dmrozik87 <dmrozik87@gmail.com>
2023-03-28 08:04:57 +0200

    Added welcome text on header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,12 @@
 import "./Header.css";
 import { Link } from 'react-router-dom';
 
-function Header({ isUserLogged, setIsUserLogged }) {
+function Header({ isUserLogged, setIsUserLogged, userData, setUserData }) {
+
+  function logout() {
+    setIsUserLogged(false);
+    setUserData("");
+  }
   
   return (
     <div className="header">
@@ -9,12 +14,13 @@ function Header({ isUserLogged, setIsUserLogged }) {
       <h1 className="header_name">Find&Go</h1>
       </Link>
       <div>
+        {userData !== "" && <h3 className="welcome">Welcome, {userData.firstName}!</h3>}
         <Link to="/about">
           <button className="header_button">About</button>
         </Link>
         {isUserLogged && <Link to="/favourites"><button className="header_button">Favourites</button></Link>}
         {!isUserLogged && <Link to="/login"><button className="header_button" >Log In</button></Link>}
-        {isUserLogged && <Link to="/"><button className="header_button" onClick={() => setIsUserLogged(false)}>Log Out</button></Link>}
+        {isUserLogged && <Link to="/"><button className="header_button" onClick={logout}>Log Out</button></Link>}
       </div>
     </div>
   )
